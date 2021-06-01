afb6e4cf07e0ba45ac84fe198809b835e25797f5
Khanh Vu <kh697642@dal.ca>
2021-06-01 17:33:50 +0700

    Fixing errors when deploying on Chrome browser. Having uncaughtType from browserify when implementing youtube-dl

---
+++ Hello World Extension/popup.js
@@ -0,0 +1,17 @@
+
+const youtubedl = require('youtube-dl-exec')
+
+document.getElementById("body").addEventListener("click", myFunction);
+
+function myFunction() {
+  youtubedl('https://www.youtube.com/watch?v=wHZCYi1K664&t=1352s', {
+  dumpSingleJson: true,
+  noWarnings: true,
+  noCallHome: true,
+  noCheckCertificate: true,
+  preferFreeFormats: true,
+  youtubeSkipDashManifest: true,
+  referer: 'https://www.youtube.com/watch?v=wHZCYi1K664&t=1352s'
+})
+  .then()
+}
